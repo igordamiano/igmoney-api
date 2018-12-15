3b72cd99cbdab5c08989665ae5b3e41edd52c2eb
irdbr <irdbr@IRDBR>
2018-12-15 09:58:20 -0200

    1

---
+++ src/main/java/com/igormoney/api/IgmoneyApiApplication.java
@@ -0,0 +1,14 @@
+package com.igormoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgmoneyApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/igormoney/api/IgmoneyApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.igormoney.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IgmoneyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
