b69e0e68ea5a9e0f94e3dda39ccb0310ca431009
irdbr <irdbr@IRDBR>
2019-01-27 11:48:20 -0200

    1

--- src/main/java/com/igormoney/api/resource/CategoriaResource.java
+++ src/main/java/com/igormoney/api/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.igormoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.igormoney.api.model.Categoria;
 import com.igormoney.api.repository.CategoriaRepoitory;
@@ -21,5 +30,24 @@ public class CategoriaResource {
 	public List<Categoria> listar(){
 		return categoriaRepoitory.findAll();
 	}
+	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED) -- Não precisa mais da anotação para Status Usando o retorno ResponseEntity<Categoria> 
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepoitory.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		
+		return categoriaRepoitory.findById(codigo);
+	}
+	
 
 }
