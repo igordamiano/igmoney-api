7c408ac6088571c95cb039127be984fd902f5cab
irdbr <irdbr@IRDBR>
2019-01-29 18:43:22 -0200

    1

--- src/main/java/com/igormoney/api/resource/CategoriaResource.java
+++ src/main/java/com/igormoney/api/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,7 +34,7 @@ public class CategoriaResource {
 	
 	@PostMapping
 	//@ResponseStatus(HttpStatus.CREATED) -- Não precisa mais da anotação para Status Usando o retorno ResponseEntity<Categoria> 
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepoitory.save(categoria);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
