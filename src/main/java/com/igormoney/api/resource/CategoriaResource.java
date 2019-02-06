efad6f66247a7c39c1e67c17937f8b75b7068e47
irdbr <irdbr@IRDBR>
2019-02-06 20:48:57 -0200

    1

--- src/main/java/com/igormoney/api/resource/CategoriaResource.java
+++ src/main/java/com/igormoney/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.igormoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.igormoney.api.event.RecursoCriadoEvent;
 import com.igormoney.api.model.Categoria;
 import com.igormoney.api.repository.CategoriaRepository;
 
@@ -27,6 +28,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepoitory;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher; 
+	
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepoitory.findAll();
@@ -36,14 +40,11 @@ public class CategoriaResource {
 	//@ResponseStatus(HttpStatus.CREATED) -- Não precisa mais da anotação para Status Usando o retorno ResponseEntity<Categoria> 
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepoitory.save(categoria);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
+		
 	@GetMapping("/{codigo}")
 	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		
